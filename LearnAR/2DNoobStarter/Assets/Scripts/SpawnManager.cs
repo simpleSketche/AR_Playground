6d1b74050a11ecff75932a778d01bcb747f0415a
simple sketch <71196100+simpleSketche@users.noreply.github.com>
2022-11-28 15:51:46 -0500

    creatge prefab and obstacles

---
+++ LearnAR/2DNoobStarter/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] redObstacles;
+    public GameObject[] blueObstacles;
+
+    private float LL = -4.05f;
+    private float LR = -1.35f;
+    private float RL = 1.35f;
+    private float RR = 4.05f;
+}
